f3ee863439fc8a9e9695139c8e0af00969ec2be8
Harry Han <s2162783@ed.ac.uk>
2024-04-04 09:25:24 +0100

    lux fiat

---
+++ build.py
@@ -0,0 +1,45 @@
+import json
+import os
+
+file_names = [
+    "./page_rank_info.ipynb",
+    "./power_iteration.ipynb",
+    "./markov_matrix.ipynb"
+]
+
+build_destination = "./build/project.ipynb"
+
+
+def write_components_to_file(file_name, components):
+    # create directory
+    if not os.path.exists(os.path.dirname(file_name)):
+        os.makedirs(os.path.dirname(file_name))
+
+    f = open(file_name, "w")
+    f.write(json.dumps(components))
+    f.close()
+
+
+final_json = {}
+cells = []
+
+
+f = open(file_names[2], "r")
+j = json.load(f)
+f.close()
+
+
+for i in file_names:
+    f = open(i, "r")
+    j = json.load(f)
+    f.close()
+    cells += j["cells"]
+
+
+final_json["cells"] = cells
+for i in j:
+    if i == "cells":
+        continue
+    final_json[i] = j[i]
+
+write_components_to_file(build_destination, final_json)
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Honours Algebra Project
+
+To build the final project from the three files, run: 
+
+```
+python build.py
+```
+
+The final project will be presented in `\build` directory. From whence you can upload to Noteable.
+
+Please edit you files presented in the root directory.
